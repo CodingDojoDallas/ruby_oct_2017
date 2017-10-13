646ca9c573fff39b52c4276a39ca1e3eb1fecab8
Alyssa McKee <alyssa_mckee@yahoo.com>
2017-10-12 21:43:28 -0500

    I accidentally pastedthe last git directly to the terminal and it tried to run a ton of 'commands' and I think it broke things but I'm to scared to git reset so i'm just gonna revert back to the last working commit immediately after this

--- Alyssa_McKee/TTD/rspeconrails/WALLS/config/routes.rb
+++ Alyssa_McKee/TTD/rspeconrails/WALLS/config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
 
-root 											'user#index'
+get root 'user#index'
 
-get 'users' 								=> 	'user#index', 		as: 'landing'
+get 'users' => 'user#index', as: 'landing'
 
-post 'users' 								=> 	'user#create', 		as: 'register'
+post 'users' => 'user#create', as: 'register'
 
-post 'users/login' 							=> 	'user#login', 		as: 'login'
+post 'users/login' => 'user#login', as: 'login'
 
-get 'users/logout' 							=> 	'user#logout', 		as: 'logout'
+get 'users/logout' => 'user#logout', as: 'logout'
 
-post 'walls/:name/messeges/:id/comments' 	=> 	'comments#create', 	as: 'create_comment' #create comment
+post 'walls/:name/messeges/:id/comments' => 'comments#create', as: 'create_comment' #create comment
 
-post 'walls/:name/messages' 				=> 	'messeges#create', 	as: 'create_message' #create messege
+post 'walls/:name/messages' => 'messeges#create', as: 'create_message' #create messege
 
-get 'walls/:name' 							=> 	'walls#index', 		as: 'wall' #show all for that wall
+get 'walls/:name' => 'walls#index', as: 'wall' #show all for that wall
+
 
 end
