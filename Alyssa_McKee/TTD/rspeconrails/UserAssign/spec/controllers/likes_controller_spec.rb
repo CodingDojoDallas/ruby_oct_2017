ff9338946ddf01813b2b3d541815f0f727b40174
Alyssa McKee <alyssa_mckee@yahoo.com>
2017-10-17 15:20:40 -0500

    likes authorization done

--- Alyssa_McKee/TTD/rspeconrails/UserAssign/spec/controllers/likes_controller_spec.rb
+++ Alyssa_McKee/TTD/rspeconrails/UserAssign/spec/controllers/likes_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 RSpec.describe LikesController, type: :controller do 
   before do 
     @user = create(:user)
+	
     @secret = create(:secret, user: @user)
     @like = create(:like, secret: @secret, user: @user)
   end
@@ -19,4 +20,20 @@ RSpec.describe LikesController, type: :controller do
 		
 	end
   end
+  before do
+	@user = create(:user)
+	session[:user_id] = @user.id
+	@secret = create(:secret, user: @user)
+	
+	@user2 = create(:user, :female)
+    @like = create(:like, secret: @secret, user: @user2)
+  end
+  context "when signed in as the wrong user" do
+    it "shouldn't be able to destroy a like" do
+		@request.env["HTTP_REFERER"] = "/secrets"
+	
+		delete :destroy, secret_id: @secret.id
+		expect(Like.where(id: @like.id).any?).to be(true)
+	end
+  end 
 end
